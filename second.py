0f805c2a8749f41eec930e7c93b3cead091ca270
AnastasiaV <anastasia.l.v@yandex.ru>
2013-10-15 22:08:00 +0700

    changed

--- second.py
+++ second.py
@@ -1,25 +1,21 @@
 import random
 
-def testForInclusion(name, namefile):
+def test_for_inclusion(name,namefile):
     name+='\n'
     with open(namefile,'r') as myfile:
         for element in myfile:
-            print(repr(name))
-            print(repr(element))
-            print()
             if name==element:
                 return 1
     return 0
 
 
-def newNames(firstname,namefile):
+def new_names(firstname,namefile):
     counter=0
     names=['0','0','0','0']
-    while counter<4:
+    while counter<4: 
         number=random.randint(0,100)
-        number=str(number)
-        secondname=firstname+number
-        doubling=testForInclusion(secondname,namefile)
+        secondname=firstname+str(number)
+        doubling=test_for_inclusion(secondname,namefile)
         if doubling==0:
             doubling0=0
             for element in names:
@@ -34,17 +30,16 @@ def newNames(firstname,namefile):
 def main():
     firstname=input('Введите имя\n')
     namefile='list_names.txt'
-    doubling=testForInclusion(firstname,namefile)
+    doubling=test_for_inclusion(firstname,namefile)
     print(doubling)
     doubling0=0
 
     if doubling==1:
-        names=newNames(firstname,namefile)
+        names=new_names(firstname,namefile)
         print("Такое имя уже существует. Выберите из предложенных\n",names)
         firstname=input()
         for element in names:
             if firstname==element:
-
                 doubling0=0
                 break
             else:
@@ -57,8 +52,7 @@ def main():
     else:
         print('Пользователь НЕ зарегестрирован')
                 
-                  
-        
-if __name__ == '__main__':
+
+if __name__=='__main__':
     main()
     
