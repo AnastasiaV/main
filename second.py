5dc16a309844e158875d06682ec2edb3556c48ca
Anastasia Lyubich <anstasia.l.v@yandex.ru>
2013-10-15 18:54:12 +0700

    changed

--- second.py
+++ second.py
@@ -1,28 +1,25 @@
 import random
 
-def TestForInclusion(name,namefile):
-    file=open(namefile,'r+')
-    doubling=0
-    listname=file.readlines()
-    file.close()
-
-    changedname=name+'\n'
-    for element in listname:
-        if changedname==element:
-            doubling=1
-            break    
-
-    return doubling
+def testForInclusion(name, namefile):
+    name+='\n'
+    with open(namefile,'r') as myfile:
+        for element in myfile:
+            print(repr(name))
+            print(repr(element))
+            print()
+            if name==element:
+                return 1
+    return 0
 
 
-def NewNames(firstname,namefile):
+def newNames(firstname,namefile):
     counter=0
     names=['0','0','0','0']
     while counter<4:
         number=random.randint(0,100)
         number=str(number)
         secondname=firstname+number
-        doubling=TestForInclusion(secondname,namefile)
+        doubling=testForInclusion(secondname,namefile)
         if doubling==0:
             doubling0=0
             for element in names:
@@ -37,12 +34,12 @@ def NewNames(firstname,namefile):
 def main():
     firstname=input('Введите имя\n')
     namefile='list_names.txt'
-    doubling=TestForInclusion(firstname,namefile)
+    doubling=testForInclusion(firstname,namefile)
     print(doubling)
     doubling0=0
 
     if doubling==1:
-        names=NewNames(firstname,namefile)
+        names=newNames(firstname,namefile)
         print("Такое имя уже существует. Выберите из предложенных\n",names)
         firstname=input()
         for element in names:
@@ -54,10 +51,8 @@ def main():
                 doubling0=1
 
     if doubling0==0:
-        file=open('list_names.txt','a')
-        firstname+='\n'
-        file.write(firstname)
-        file.close()
+        with open('list_names.txt','a') as myfile:
+            print (firstname, file=myfile)
         print('Пользователь зарегистрирован')
     else:
         print('Пользователь НЕ зарегестрирован')
