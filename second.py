851320a17130de16f4de32191955af193ac61ac2
Anastasia Lyubich <anstasia.l.v@yandex.ru>
2013-10-22 17:46:10 +0700

    changed 2

--- second.py
+++ second.py
@@ -1,10 +1,9 @@
 import random
 
 def test_for_inclusion(name,namefile):
-    name+='\n'
     with open(namefile,'r') as myfile:
         for element in myfile:
-            if name==element:
+            if name==element.rstrip():
                 return 1
     return 0
 
@@ -30,27 +29,32 @@ def new_names(firstname,namefile):
 def main():
     firstname=input('Введите имя\n')
     namefile='list_names.txt'
-    doubling=test_for_inclusion(firstname,namefile)
-    print(doubling)
-    doubling0=0
+    try:
+        filefirst=open(namefile,'r')
+        exept :
+            filefirst=open(namefile,'w')
 
-    if doubling==1:
-        names=new_names(firstname,namefile)
-        print("Такое имя уже существует. Выберите из предложенных\n",names)
-        firstname=input()
-        for element in names:
-            if firstname==element:
-                doubling0=0
-                break
+            doubling=test_for_inclusion(firstname,namefile)
+            print(doubling)
+            doubling0=0
+
+            if doubling==1:
+            names=new_names(firstname,namefile)
+            print("Такое имя уже существует. Выберите из предложенных\n",names)
+            firstname=input()
+            for element in names:
+                if firstname==element:
+                    doubling0=0
+                    break
+                else:
+                    doubling0=1
+
+            if doubling0==0:
+                with open('list_names.txt','a') as myfile:
+                    print (firstname, file=myfile)
+                    print('Пользователь зарегистрирован')
             else:
-                doubling0=1
-
-    if doubling0==0:
-        with open('list_names.txt','a') as myfile:
-            print (firstname, file=myfile)
-        print('Пользователь зарегистрирован')
-    else:
-        print('Пользователь НЕ зарегестрирован')
+                print('Пользователь НЕ зарегестрирован')
                 
 
 if __name__=='__main__':
